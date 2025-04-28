da55351d93aee72866445d95fb83d30031203249
Dmitry Andrievsky <dandrievsky@gmail.com>
2025-04-28 11:14:59 +0300

    initial

---
+++ hyperrogue-debian12/build.sh
@@ -0,0 +1,9 @@
+apt update
+apt install make g++ libsdl1.2-dev libsdl-ttf2.0-dev libsdl-gfx1.2-dev libsdl-mixer1.2-dev libglew-dev
+
+apt install curl
+curl -LOJ "https://github.com/zenorogue/hyperrogue/archive/refs/tags/v13.0x.tar.gz"
+tar -xf hyperrogue-13.0x.tar.gz
+cd hyperrogue-13.0x
+
+HYPERROGUE_USE_GLEW=1 HYPERROGUE_USE_PNG=1 make
