7379ba52cff0c42f63b8793719ca231f9fb2ecfb
Dmitry Andrievsky <dandrievsky@gmail.com>
2025-05-02 23:58:02 +0300

    vkQuake

--- hyperrogue-debian12/build.sh
+++ hyperrogue-debian12/build.sh
@@ -1,10 +1,5 @@
 sudo apt update
 sudo apt install make g++ libsdl1.2-dev libsdl-ttf2.0-dev libsdl-gfx1.2-dev libsdl-mixer1.2-dev libglew-dev
 
-# apt install curl
-# curl -LOJ "https://github.com/zenorogue/hyperrogue/archive/refs/tags/v13.0x.tar.gz"
-#tar -xf hyperrogue-13.0x.tar.gz
-
-ls -lah
-cd hyperrogue
+cd project
 HYPERROGUE_USE_GLEW=1 HYPERROGUE_USE_PNG=1 make
---
+++ vkQuake-m1/build.sh
@@ -0,0 +1,4 @@
+brew install molten-vk vulkan-headers glslang spirv-tools sdl2 libvorbis flac opus opusfile flac mpg123 meson pkgconfig
+
+cd project
+meson build && ninja -C build
