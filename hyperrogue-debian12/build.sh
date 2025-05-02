b49cee9cf3adb2b12da7fe959e452f0402e57f4f
Dmitry Andrievsky <dandrievsky@gmail.com>
2025-05-02 19:41:29 +0300

    fix1

--- hyperrogue-debian12/build.sh
+++ hyperrogue-debian12/build.sh
@@ -1,9 +1,10 @@
-apt update
-apt install make g++ libsdl1.2-dev libsdl-ttf2.0-dev libsdl-gfx1.2-dev libsdl-mixer1.2-dev libglew-dev
+sudo apt update
+sodo apt install make g++ libsdl1.2-dev libsdl-ttf2.0-dev libsdl-gfx1.2-dev libsdl-mixer1.2-dev libglew-dev
 
 # apt install curl
 # curl -LOJ "https://github.com/zenorogue/hyperrogue/archive/refs/tags/v13.0x.tar.gz"
 #tar -xf hyperrogue-13.0x.tar.gz
 
-cd ../hyperrogue
+ls -lah
+cd hyperrogue
 HYPERROGUE_USE_GLEW=1 HYPERROGUE_USE_PNG=1 make
